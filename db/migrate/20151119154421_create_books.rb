d21d9bbfd8ba2f3836e3708f9d3c1933e770c5a4
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-11-19 13:48:36 -0200

    Update Book's migration file

--- db/migrate/20151119154421_create_books.rb
+++ db/migrate/20151119154421_create_books.rb
@@ -1,11 +1,11 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.datetime :publish_date
-      t.string :isbn
-      t.references :author, index: true, foreign_key: true
-      t.references :tag, index: true, foreign_key: true
+      t.string :isbn, unique: true
+      t.references :author, index: true, foreign_key: true, null: false
+      t.references :tag, index: true, foreign_key: true, null: false
 
       t.timestamps null: false
     end
